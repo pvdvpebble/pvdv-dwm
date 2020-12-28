d446a59f09c54abe2c6915bc703d356c676fed35
Paul van de Ven <paul@gutstickets.org>
2020-12-28 19:02:48 +0100

    styling tweaks

--- config.def.h
+++ config.def.h
@@ -10,17 +10,33 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const unsigned int gappx     = 2;        /* gap pixel between windows */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "MesloLGS NF:size=10" };
+static const char dmenufont[]       = "MesloLGS NF:size=10";
+
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+
+/* Dracula Colors */
+static const char col_black1[] 		= "#000000";
+static const char col_black2[] 		= "#44475a";
+static const char col_red[] 		= "#ff5555";
+static const char col_green[] 		= "#50fa7b";
+static const char col_yellow[] 		= "#f1fa8c";
+static const char col_blue[] 		= "#bd93f9";
+static const char col_magenta[] 	= "#ff79c6";
+static const char col_cyan[] 		= "#8be9fd";
+static const char col_white1[]		= "#bbbbbb";
+static const char col_white2[] 		= "#ffffff";
+
+static const char col_bg[]			= "#282a36";
+static const char col_fg[]			= "#f8f8f2";
+
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_fg, col_bg, col_bg },
+	[SchemeSel]  = { col_white2, col_black2, col_magenta  },
 };
 
 /* tagging */
@@ -61,7 +77,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_white1, "-sb", col_magenta, "-sf", col_fg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
